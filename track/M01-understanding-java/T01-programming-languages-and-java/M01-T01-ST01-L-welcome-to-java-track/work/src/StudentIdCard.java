78e5c0cb2cb482568136222171971535cb073109
mukeshjinka <mukeshjinka6@gmail.com>
2026-09-06 17:12:55 +0530

    StudentIdCard

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/StudentIdCard.java
@@ -0,0 +1,11 @@
+public class StudentIdCard {
+    public static void main(Strings[] args){
+        // Write your code here
+        System.out.println("--------------------");
+        System.out.println("STUDENT PROFILE");
+        System.out.println("Name: Alex");
+        System.out.println("Course: Java");
+        System.out.println("Level: Beginner");
+        System.out.println("--------------------");
+    }
+}
