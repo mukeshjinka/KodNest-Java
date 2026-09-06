f1913dc661c68c2068ef17534730358b9715e2ab
mukeshjinka <mukeshjinka6@gmail.com>
2026-09-06 17:01:23 +0530

    Added MyPlacementGoal

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/MyPlacementGoal.java
@@ -0,0 +1,9 @@
+public class MyPlacementGoal {
+    public static void main(Strings[] args){
+        // Write your code here
+        System.out.println("---MY PlACEMENT GOAL ---");
+        System.out.println("Role: Software Developer");
+        System.out.println("Target: Get Placed");
+        System.out.println("Daily Practice: 8 Hours");
+    }   
+}
