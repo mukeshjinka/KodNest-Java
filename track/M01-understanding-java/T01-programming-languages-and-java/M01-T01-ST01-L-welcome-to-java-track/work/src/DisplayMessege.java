d680c8da9446e20b0f351f4d96d60c87c3dd1977
mukeshjinka <mukeshjinka6@gmail.com>
2026-09-23 15:28:17 +0530

    1st messege

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/DisplayMessege.java
@@ -0,0 +1,6 @@
+public class DisplayMessege {
+    public static void main(String[] args){
+        System.out.println("Hello KodNest");
+    }
+    
+}
---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/example.java
@@ -0,0 +1,9 @@
+class example{
+    public static void main (String[] args){
+        while (i==1){
+            i<=5;
+            i++;
+            System.out.println(i);
+        }
+    }
+}
