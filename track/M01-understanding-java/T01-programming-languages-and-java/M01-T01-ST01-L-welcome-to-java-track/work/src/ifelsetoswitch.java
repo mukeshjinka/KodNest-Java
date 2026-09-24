30106dfdbabdc783d58d5a5426bee74a609c6cd3
mukeshjinka <mukeshjinka6@gmail.com>
2026-09-24 13:42:11 +0530

    ifelseswitch

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/ifelsetoswitch.java
@@ -0,0 +1,12 @@
+public class ifelsetoswitch {
+    public static void main(String[] args){
+int day = 6;
+if (day >= 1 && day <= 5) {
+System.out.println("Learning Day");
+} else if (day == 6 || day ==7) {
+System.out.println("Revision Day");
+} else {
+System.out.println("Invalid Day");
+    }
+}
+}
