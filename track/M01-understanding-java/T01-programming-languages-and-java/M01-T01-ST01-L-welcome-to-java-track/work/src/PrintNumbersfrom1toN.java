d18c32920726446255c4bb1d20a80042f1c1fd46
mukeshjinka <mukeshjinka6@gmail.com>
2026-09-24 17:42:41 +0530

    1 to N

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/PrintNumbersfrom1toN.java
@@ -0,0 +1,9 @@
+public class PrintNumbersfrom1toN {
+    public static void main(String[] args){
+        int limit = 5;
+        // Print numbers from 1 through limit
+        for (int i=1 ; i <= limit ; i++){
+            System.out.println(i);
+        }
+    }
+}
