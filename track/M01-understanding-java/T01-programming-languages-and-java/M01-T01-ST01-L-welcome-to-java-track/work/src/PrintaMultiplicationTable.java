cf41dff34561db34181099da6157d8818c0473d7
mukeshjinka <mukeshjinka6@gmail.com>
2026-09-24 17:46:55 +0530

    tables

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/PrintaMultiplicationTable.java
@@ -0,0 +1,10 @@
+public class PrintaMultiplicationTable {
+    public static void main(String[] args) {
+        int number = 5;
+        // Print the first five multiples
+        for (int i = 1 ; i <=5 ; i++){
+            int x = 5 * i;
+            System.out.println("5 " + "x " + i + " = "+ x );
+        }
+    }
+}
