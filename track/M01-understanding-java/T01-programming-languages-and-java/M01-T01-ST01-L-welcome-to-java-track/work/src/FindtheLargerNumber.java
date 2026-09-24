dbeff5b93b96af588ca63e8433a3cd440e19c6d7
mukeshjinka <mukeshjinka6@gmail.com>
2026-09-24 17:33:33 +0530

    largerno.

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/FindtheLargerNumber.java
@@ -0,0 +1,11 @@
+public class FindtheLargerNumber {
+    public static void main (String[] args){
+        int first = 25;
+        int second = 40;
+     // Compare the two values        
+     if (first > second){            
+        System.out.println("Larger: " + second);        
+    }else if (first < second){            
+        System.out.println("Larger: " + second);        }
+    }
+}
