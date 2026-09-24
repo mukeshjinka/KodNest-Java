d481e73e6f54fe9cdbf2ebfeeecfb6af9e3ce7c8
mukeshjinka <mukeshjinka6@gmail.com>
2026-09-24 17:30:23 +0530

    evenorodd

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/CheckEvenorOdd.java
@@ -0,0 +1,11 @@
+public class CheckEvenorOdd{
+    public static void main(String[] args){
+        int number = 14;
+        // Check whether number is even or odd
+        if (number % 2 == 0){
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
