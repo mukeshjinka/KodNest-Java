7fefed05b52415941c4ce9f9154b097dce81dc4f
mukeshjinka <mukeshjinka6@gmail.com>
2026-09-24 17:22:09 +0530

    checkpositiveornegative

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/CheckPositiveorNegative.java
@@ -0,0 +1,11 @@
+public class CheckPositiveorNegative {
+    public static void main (String[] args){
+        int number = -8;
+        // Check whether number is positive, negative or zero
+        if (number > 0){
+            System.out.println("Positive");
+        }else if (number < 0){
+            System.out.println("Negative");
+        }
+    }
+}
